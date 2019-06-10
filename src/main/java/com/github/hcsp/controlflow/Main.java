6d7251e4967001a54d24f2378ba4061379a08d5b
Administrator <908380434@qq.com>
2019-06-10 23:19:57 +0800

    judge 闰年

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -11,7 +11,7 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if(year % 100 == 0 && year % 400 == 0) {
+        if((year % 100 == 0) && (year % 400 == 0)) {
             return true;
         }else if(year % 4 ==0) {
             return true;
