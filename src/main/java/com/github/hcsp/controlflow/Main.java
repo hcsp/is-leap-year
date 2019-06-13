0998f5a43e19a7b4d3b5d4c360700d24f519412e
zhangyongwang <20225614@qq.com>
2019-06-13 11:11:24 +0800

    判断是否是闰年

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -11,13 +11,16 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0 && year % 400 == 0) {
-            return true;
-        } else if (year % 4 == 0) {
-            return true;
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
         } else {
-            return false;
+            if (year % 4 == 0) {
+                return true;
+            }
         }
+        return false;
     }
 
     public static void main(String[] args) {
