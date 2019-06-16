dfd6226610d67547ceb595d006901f54eb0be87e
liboyang <huagongyilby@126.com>
2019-06-16 13:36:31 +0800

    判断闰年（修改冲突）

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(isLeapYear(1999));
-        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2010));
         System.out.println(isLeapYear(2004));
     }
 }
