6e0bf055ff2b16cf6878ee8c38681fdb085c313a
lis <liyu.italy@hotmail.com>
2019-06-16 20:37:19 +0800

    modify isLeapYear()

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -11,9 +11,9 @@ public class Main {
      */
     public static boolean isLeapYear(int year) {
         if (year%100 == 0) {
-            return (year%400 == 0) ? true : false ;
+            return (year%400 == 0) ? true : false;
         }
-        else return (year%4 == 0) ? true : false ;
+        else return (year%4 == 0) ? true : false;
     }
 
     public static void main(String[] args) {
