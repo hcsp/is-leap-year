6c9ee6d4b5803ceb16d039c0fe22ed545c2ab78a
dflk <904885508@qq.com>
2019-06-27 23:15:54 +0800

    心细

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,7 +10,9 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if(year % 100 == 0 && year % 400 == 0){
+        if(year % 100 == 0  ){
+            return   year % 400 == 0 ? true : false;
+        }else if(year % 4 == 0 ){
             return true;
         }else{
             return false;
