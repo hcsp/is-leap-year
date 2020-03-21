293b8134287b74a76c2e7a47f2727076c4e6acf4
gofocus <go_focus@live.cn>
2020-03-21 16:02:55 +0800

    判断一个年份是否是闰年

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,12 +12,12 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if ((year % 100 == 0) && (year % 400 == 0)) {
-            return true;
-        } else if ((year % 100 != 0) && (year % 4 == 0)) {
-            return true;
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        } else {
+            return year % 4 == 0;
         }
-        return false;
+
     }
 
     public static void main(String[] args) {
