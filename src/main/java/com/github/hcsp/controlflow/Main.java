9c341f7472247884da4d1aaa90cbd523864ee9bf
dingw <dingweibing@outlook.jp>
2020-05-05 01:33:59 +0900

    my commit

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,11 +10,11 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if(year % 100 == 0){
-            if (year % 400 == 0) return true;
+        if (year % 100 == 0 ) {
+            return year % 400 == 0;
+        }else{
             return year % 4 == 0;
         }
-        return false;
     }
 
     public static void main(String[] args) {
