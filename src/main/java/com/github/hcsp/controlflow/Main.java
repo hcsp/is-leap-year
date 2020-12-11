a324abf9ce35481fd77a93cd131dbd7277cd975c
niuweizhe <niu853273494@qq.com>
2020-12-11 11:09:31 +0800

    leap year again

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,12 +10,12 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0 && year % 400 == 0) {
+        if (year % 400 == 0) {
             return true;
 
         }
 
-        if (year % 4 == 0) {
+        if (year % 4 == 0 && year % 100 != 0) {
             return true;
         }
 
